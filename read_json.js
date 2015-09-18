dd9932a3143e8bb02057bcd933233e5283b93946
lucifly <xilinjuebi@163.com>
2015-09-18 18:56:16 +0800

    init

--- read_json.js
+++ read_json.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var mess='{     "info": {       "author": "meemoo",       "title": "Untitled",       "description": "Meemoo app description",       "parents": [],       "url": ""     },     "nodes": [       {         "id": 0,         "src": "file:///I:/nodejsstu/iframework-master/src/nodes/mytry/toshow.html",         "x": 736,         "y": 140,         "w": 200,         "h": 210,         "state": {}       },       {         "id": 1,         "src": "file:///I:/nodejsstu/iframework-master/src/nodes/mytry/fengzhuangjiekou.html",         "x": 334,         "y": 169,         "w": 200,         "h": 210,         "state": {}       },       {         "id": 2,         "src": "file:///I:/nodejsstu/iframework-master/src/nodes/mytry/sendnum.html",         "x": 100,         "y": 307,         "w": 200,                  "h": 210,         "state": {           "square": 11         }       }     ],     "edges": [       {         "source": [           2,           "squared"         ],         "target": [           1,           "input_t"         ]       },       {         "source": [           2,           "ready"         ],         "target": [           1,           "is_ready"         ]       },       {         "source": [           1,           "output_t"         ],         "target": [           0,           "square"         ]       }     ]   }';
+var mess='{     "info": {       "author": "meemoo",       "title": "Untitled",       "description": "Meemoo app description",       "parents": [],       "url": ""     },     "nodes": [       {         "id": 0,         "src": "file:///D:/nodejsstu/iframework-master/src/nodes/mytry/toshow.html",         "x": 736,         "y": 140,         "w": 200,         "h": 210,         "state": {}       },       {         "id": 1,         "src": "file:///D:/nodejsstu/iframework-master/src/nodes/mytry/fengzhuangjiekou.html",         "x": 334,         "y": 169,         "w": 200,         "h": 210,         "state": {}       },       {         "id": 2,         "src": "file:///D:/nodejsstu/iframework-master/src/nodes/mytry/sendnum.html",         "x": 100,         "y": 307,         "w": 200,                  "h": 210,         "state": {           "square": 11         }       }     ],     "edges": [       {         "source": [           2,           "squared"         ],         "target": [           1,           "input_t"         ]       },       {         "source": [           2,           "ready"         ],         "target": [           1,           "is_ready"         ]       },       {         "source": [           1,           "output_t"         ],         "target": [           0,           "square"         ]       }     ]   }';
 var obj=JSON.parse(mess);
 
 //console.log((obj.info).author);
@@ -122,11 +122,12 @@ for(i=0; i<node_num; i++)
 {
     var file_path = (((my_node[i]).src)) ;
     
-    var t1 = file_path.indexOf('I:/') ;
+    var t1 = file_path.indexOf('D:/') ;
     var t2 = file_path.indexOf('.html') ;
     var t3 = file_path.substr(t1,t2- t1);
     
     var my_f_json = "a:";
+	//console.log(t3);
     my_f_json = fs.readFileSync( t3+'.json', 'utf8', function (err, data ) {
         if (err) throw err;
     });
@@ -347,7 +348,7 @@ write_to_file =var_idfi +"   "+rererer/*+ somewords_O_arr*/ + tttttt+ t123 + tet
 
     console.log(write_to_file);
  //写入文件
- fs.writeFile('I:/nodejsstu/iframework-master/src/nodes/mytry/tst.json', write_to_file, function (err) {
+ fs.writeFile('D:/nodejsstu/iframework-master/src/nodes/mytry/tst.json', write_to_file, function (err) {
     if (err) throw err;
     console.log('It\'s saved!'); //文件被保存
 });   
